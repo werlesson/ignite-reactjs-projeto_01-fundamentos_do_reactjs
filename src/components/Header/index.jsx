df8251be8bb778f361a537a709a19d5b7d87b890
werlesson <werlessono@gmail.com>
2023-06-20 10:54:16 -0300

    style: custom header component

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import styles from './styles.module.css'
 
+import igniteLogo from '../../assets/ignite-logo.svg';
+
 export function Header() {
     return (
         <header className={styles.header}>
-            <strong>teste</strong>
+            <img src={igniteLogo} alt="Logotipo da aplicação" />
+            <strong>Ignite Feed</strong>
         </header>
     )
 }
