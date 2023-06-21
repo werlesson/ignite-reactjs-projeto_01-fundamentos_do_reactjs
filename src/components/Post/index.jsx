137373f761ce28a337aa3f4f65864a99bccdb102
werlesson <werlessono@gmail.com>
2023-06-21 00:47:04 -0300

    feat: refact post component and custom

--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -1,8 +1,29 @@
-export function Post({author, content}) {
+import styles from './style.module.css'
+
+export function Post() {
     return (
-        <section>
-            <h3>{author}</h3>
-            <article>{content}</article>
-        </section>
+        <article className={styles.post}>
+            <header>
+                <div className={styles.author}>
+                    <img className={styles.avatar} src="https://github.com/werlesson.png" />
+                    <div className={styles.authorInfo}>
+                        <strong>Werlesson Vieira</strong>
+                        <span>Web Developer</span>
+                    </div>
+                </div>
+                <time title='21 de Junho às 21:45h' dateTime='2023-06-21 21:45:53'>Publicado há 1h</time>
+            </header>
+
+            <div className={styles.content}>
+                <p>Fala galeraa 👋</p>
+                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+                <p>👉  <a href="#">jane.design/doctorcare</a></p>
+                <p>
+                    <a href="#">#novoprojeto</a>
+                    <a href="#">#nlw</a>
+                    <a href='#'>#rocketseat</a>
+                </p>
+            </div>
+        </article>
     )
 }
